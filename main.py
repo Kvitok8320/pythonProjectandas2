bb429caf5a8add8410d78e0e71566473463601f7
sportzal <ipkvitov@gmail.com>
2024-06-18 20:06:07 +0300

    dct

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+data = pd.read_csv("Student_performance_data _.csv")
+
+df = pd.DataFrame(data)
+print(df.head(3))
+print(df.info())
+print(df.describe())
+print(f"средний возраст {df['Age'].mean()}")
+
+data2 = pd.read_csv("dz.csv")
+df2=pd.DataFrame(data2)
+mean_salary = df2.groupby("City")["Salary"].mean()
+print(f'Средняя зарплата по городам {mean_salary}')
+
+
+data3 = pd.read_csv("Sheet1.csv")
+df3=pd.DataFrame(data3)
+print(df3.head())
+print(df3.info())
+print(df3.describe())
+#mean_estimate = df3.mean()
+print(df3.select_dtypes(include='number').mean())
+
